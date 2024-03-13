e1323757c341d73d1b4eaf44c68a58f6a0014ad9
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-12 19:21:34 -0600

    ESpecificando target y source

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -15,7 +15,8 @@ bool compareSpeed(Enemy *a, Enemy *b) {
     return a->getSpeed() > b->getSpeed();
 }
 
-Player::Player(string name, int health, int attack, int defense, int speed) : Character(name, health, attack, defense, speed, true) {
+Player::Player(string name, int health, int attack, int defense, int speed) : Character(name, health, attack, defense,
+                                                                                        speed, true) {
     experience = 0;
     level = 1;
 }
@@ -26,33 +27,31 @@ void Player::doAttack(Character *target) {
     target->takeDamage(trueDamage);
 }
 
-void Player:: die(){
-    cout<<"You die"<<endl;
-    this->dead=true;
+void Player::die() {
+    cout << "You die" << endl;
+    this->dead = true;
 }
 
 void Player::takeDamage(int damage) {
     setHealth(health - damage);
 
-    if(health <= 0) {
+    if (health <= 0) {
         die();
-    }
-    else {
-        cout<<"You have taken " << damage << " damage" << endl;
+    } else {
+        cout << "You have taken " << damage << " damage" << endl;
     }
 }
 
-bool Player::flee(vector<Enemy*> enemies) {
+bool Player::flee(vector<Enemy *> enemies) {
     std::sort(enemies.begin(), enemies.end(), compareSpeed);
-    Enemy* fastestEnemy = enemies[0];
+    Enemy *fastestEnemy = enemies[0];
     bool fleed = false;
-    if(this->getSpeed() > fastestEnemy->getSpeed()) {
-        fleed =  true;
-    }
-    else {
+    if (this->getSpeed() > fastestEnemy->getSpeed()) {
+        fleed = true;
+    } else {
         srand(time(NULL));
         int chance = rand() % 100;
-        cout<< "chance: " << chance << endl;
+        cout << "chance: " << chance << endl;
         fleed = chance > 99;
     }
 
@@ -60,7 +59,7 @@ bool Player::flee(vector<Enemy*> enemies) {
 }
 
 void Player::emote() {
-    cout<<"Jokes on you" << endl;
+    cout << "Jokes on you" << endl;
 }
 
 void Player::levelUp() {
@@ -79,10 +78,10 @@ void Player::gainExperience(int exp) {
     }
 }
 
-Character* Player::getTarget(vector<Enemy *> enemies) {
+Character *Player::getTarget(vector<Enemy *> enemies) {
     cout << "Choose a target" << endl;
     int targetIndex = 0;
-    for(int i = 0; i < enemies.size(); i++) {
+    for (int i = 0; i < enemies.size(); i++) {
         cout << i << ". " << enemies[i]->getName() << endl;
     }
     cin >> targetIndex;
@@ -90,13 +89,13 @@ Character* Player::getTarget(vector<Enemy *> enemies) {
     return enemies[targetIndex];
 }
 
-Action Player::takeAction(vector<Enemy*>enemies) {
+Action Player::takeAction(vector<Enemy *> enemies) {
     int option = 0;
-    cout<<"Choose an action"<<endl;
-    cout<<"1. Attack"<<endl;
-    cout<<"2. Flee"<<endl;
+    cout << "Choose an action" << endl;
+    cout << "1. Attack" << endl;
+    cout << "2. Flee" << endl;
     cin >> option;
-    Character* target = nullptr;
+    Character *target = nullptr;
 
     //Esta variable guarda
     //1. Que voy a hacer?
@@ -104,23 +103,25 @@ Action Player::takeAction(vector<Enemy*>enemies) {
     Action myAction;
     //2.
     myAction.speed = this->getSpeed();
+    myAction.source = this;
 
-    switch(option) {
+    switch (option) {
         case 1:
             target = getTarget(enemies);
+            myAction.target = target;
             //1.
-            myAction.action = [this, target](){
+            myAction.action = [this, target]() {
                 doAttack(target);
             };
             break;
 
         case 2:
-            myAction.action = [this, target, &enemies](){
+            myAction.action = [this,&enemies]() {
                 flee(enemies);
             };
             break;
         default:
-            cout<<"Invalid option"<<endl;
+            cout << "Invalid option" << endl;
             break;
     }
 
